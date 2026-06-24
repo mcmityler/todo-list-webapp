2c496bd103262b9d82f42ffe66299aa0ff0992a6
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-06-23 23:51:09 -0400

    Add ability to add new projects to console list

--- src/display.js
+++ src/display.js
@@ -1,8 +1,34 @@
+import ProjectManager from "./projectManager.js"
+
 export default class Display{
     constructor(){
         this.closeNewProject = document.querySelector(".close-dialog-button");
         this.newProjectForm = document.getElementById("new-project-dialog");
-        this.closeNewProject.addEventListener("click", ()=>{this.newProjectForm.close()})
+        this.closeNewProject.addEventListener("click", ()=>{this.closeNewProjectForm()});
+        this.newProjectForm.addEventListener("submit", ()=>{this.submitNewProjectForm()});
+        this.projectManager = new ProjectManager();
+        this.openNewProject = document.querySelector(".new-project-button");
+        this.openNewProject.addEventListener("click", ()=>{this.openNewProjectForm()});
+
+    }
+
+    openNewProjectForm(){
+        this.newProjectForm.showModal();
+    }
+    closeNewProjectForm(){
+        this.newProjectForm.close()
+    }
+    submitNewProjectForm(){
+        event.preventDefault(); 
+        const formData = new FormData(event.target);
+        const projectName = formData.get('project-name-input');
+        this.projectManager.addProject(projectName);
+        this.projectManager.logProjects();
+        this.updateProjectList();
+        event.target.reset();
+        this.newProjectForm.close()
+    }
+    updateProjectList(){
 
     }
 
